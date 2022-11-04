ecdd4e498ea6ea44f853c2127c5ca08923b9a66c
hui00 <kevinbrinkschulte@hotmail.de>
2022-11-04 22:39:03 +0100

    form with formTag

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,28 @@
 class UsersController < ApplicationController
   def new
+    @user = User.new
   end
 
   def create
+
+    @user = User.new(username: params[:user][:username], email: params[:user][:email], password: params[:user][:password])
+    # @user = User.new(user_params)
+
+    if @user.save
+      puts("User saved")
+      flash[:success] = "new user created"
+      redirect_to action: "new"
+    else
+      # show errors
+      puts("User not saved: " + @user.errors.full_messages.to_s)
+      flash[:danger] = "User not saved: " + @user.errors.full_messages.to_s
+      render 'new'
+    end
+
   end
+
+  # private def user_params
+  #   params.require(:user).permit(:username, :email, :password)
+  #
+  # end
 end
--- db/migrate/20221104193203_change_colum_name.rb
+++ db/migrate/20221104193203_change_colum_name.rb
@@ -1,4 +1,5 @@
 class ChangeColumName < ActiveRecord::Migration[7.0]
   def change
+    rename_column :users, :password_disgest, :password_digest
   end
 end
